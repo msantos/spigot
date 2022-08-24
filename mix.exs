18eb1eca41a636c14edaddf1bf9a8cef6f6ba38a
Michael Santos <michael.santos@gmail.com>
2022-08-24 09:39:26 -0400

    1.1.1: deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Spigot.Mixfile do
   def project do
     [
       app: :spigot,
-      version: "1.1.0",
+      version: "1.1.1",
       elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
@@ -27,7 +27,7 @@ defmodule Spigot.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [
-      applications: [:logger, :hedwig_xmpp, :ex_rated, :runlet],
+      applications: [:logger, :hedwig_xmpp, :runlet],
       mod: {Spigot, []}
     ]
   end
@@ -44,12 +44,12 @@ defmodule Spigot.Mixfile do
   defp deps do
     [
       {:romeo, git: "https://github.com/scrogson/romeo.git", override: true},
-      {:fast_xml, "~> 1.1.29", override: true, manager: :rebar3},
+      {:fast_xml, "~> 1.1", override: true, manager: :rebar3},
       {:hedwig_xmpp, "~> 1.0.0"},
-      {:runlet, "~> 1.1"},
+      {:runlet, "~> 1.2"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
     ]
   end
 end
